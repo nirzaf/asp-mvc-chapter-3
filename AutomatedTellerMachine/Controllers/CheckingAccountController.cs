0cdc6624efba19b22be317d285cefec7d579602e
Farook M Fazrin <nirzaf1@hotmail.com>
2020-06-24 07:11:39 +0530

    Add project files.

---
+++ AutomatedTellerMachine/Controllers/CheckingAccountController.cs
@@ -0,0 +1,91 @@
+﻿using AutomatedTellerMachine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class CheckingAccountController : Controller
+    {
+        // GET: CheckingAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CheckingAccount/Details/5
+        public ActionResult Details()
+        {        
+            var checkingAccount = new CheckingAccount { AccountNumber = "0000123456", FirstName = "Michael", LastName = "Sullivan", Balance = 500 };
+            return View(checkingAccount);        
+        }
+
+        // GET: CheckingAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AutomatedTellerMachine/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace AutomatedTellerMachine.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AutomatedTellerMachine.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "AutomatedTellerMachine.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(AutomatedTellerMachine.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
